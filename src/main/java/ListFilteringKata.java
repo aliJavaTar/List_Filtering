599077ade174c472578f25d578329af4d1d46d74
alijavaboy <ali77erfagh99@gmail.com>
2022-03-15 17:39:19 +0330

    refactore code

--- src/main/java/ListFilteringKata.java
+++ src/main/java/ListFilteringKata.java
@@ -3,27 +3,33 @@ import java.util.List;
 
 public class ListFilteringKata {
 
-
     public List<Integer> integerList(List<Object> array) {
-        int count = 0;
-        List<Integer> convertToInteger = Arrays.asList(new Integer[array.size()]);
-        for (int i = 0; i < array.size(); i++) {
-            if (array.get(i) instanceof Integer) {
-                convertToInteger.set(i, (Integer) array.get(i));
-            } else count++;
-        }
+        return arrayIntegerWithoutNull(array);
+    }
 
-        List<Integer> arrayIntegerWithoutNull = Arrays.asList(new Integer[array.size() - count]);
-
+    private List<Integer> arrayIntegerWithoutNull(List<Integer> convertToInteger, List<Integer> arrayIntegerWithoutNull) {
         int counter = 0;
         for (Integer integer : convertToInteger) {
-
             if (integer != null) {
                 arrayIntegerWithoutNull.set(counter, integer);
                 ++counter;
             }
         }
-
         return arrayIntegerWithoutNull;
+    }
+
+    private List<Integer> arrayIntegerWithoutNull(List<Object> array) {
+        List<Integer> convertToInteger = Arrays.asList(new Integer[array.size()]);
+
+        int count = 0;
+        for (int i = 0; i < array.size(); i++) {
+            if (array.get(i) instanceof Integer) {
+                convertToInteger.set(i, (Integer) array.get(i));
+            } else count++;
+        }
+        List<Integer> arrayIntegerWithoutNull = Arrays.asList(new Integer[array.size() - count]);
+        return arrayIntegerWithoutNull(convertToInteger, arrayIntegerWithoutNull);
+
+
     }
 }
